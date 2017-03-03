6e9827b4cb6e903123000e8df51d8858c356d02c
Dan Mork <danmork@gmail.com>
2017-03-03 12:13:42 -0600

    Add Registrations

--- src/builders/index.js
+++ src/builders/index.js
@@ -2,10 +2,14 @@ import { resolveViewBuilder } from "./resolve-view-builder";
 import { nodeViewBuilder } from "./node-view-builder";
 import { textViewBuilder } from "./text-view-builder";
 import { textInputViewBuilder } from "./text-input-view-builder";
+import { containerViewBuilder } from "./container-view-builder";
+import { linkViewBuilder } from "./link-view-builder";
 
 export { 
   resolveViewBuilder,
   nodeViewBuilder,
   textViewBuilder,
-  textInputViewBuilder
+  textInputViewBuilder,
+  containerViewBuilder,
+  linkViewBuilder
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import * as building from "./building";
 import * as builders from "./builders";
 
+building.register("container", builders.containerViewBuilder);
+building.register("link", builders.linkViewBuilder);
 building.register("text", builders.textViewBuilder);
 building.register("text", builders.textInputViewBuilder, true);
 
