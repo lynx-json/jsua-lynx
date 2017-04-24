d87b379829bb8858e3a956ae62c06c3bea2abe81
Dan Mork <danmork@gmail.com>
2017-04-24 12:45:36 -0500

    Fix Find Nearest Element

--- src/builders/util.js
+++ src/builders/util.js
@@ -1,5 +1,5 @@
 export function findNearestElement(view, selector) {
   if (!view || view.matches("html")) return null;
   if (!selector) return null;
-  return document.querySelector(selector) || findNearestElement(view.parentElement, selector);
+  return view.querySelector(selector) || findNearestElement(view.parentElement, selector);
 }
