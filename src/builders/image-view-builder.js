f41f1b400dcce58d035e84cf3563f28eb915dd0f
Anson Goldade <anson.goldade@streamtext.net>
2017-05-23 17:30:44 -0500

    Build 'image' tag when 'image' node with 'src'

--- src/builders/image-view-builder.js
+++ src/builders/image-view-builder.js
@@ -1,16 +1,64 @@
+import url from "url";
 import { contentViewBuilder } from "./content-view-builder";
+import { getBlob } from "./content-node-helpers";
 
-export function imageViewBuilder(node) {
-  return contentViewBuilder(node).then(function (view) {
+function applyImageAttributes(node) {
+  return function (view) {
     var embeddedView = view.lynxGetEmbeddedView();
     if (!embeddedView) return view;
-    
+
     var height = parseInt(node.value.height);
     if (height) embeddedView.setAttribute("data-lynx-height", height);
 
     var width = parseInt(node.value.width);
     if (width) embeddedView.setAttribute("data-lynx-width", width);
-    
+
     return view;
-  });
+  };
+}
+
+function buildAsEmbeddedImageTag(node) {
+  var view = document.createElement("div");
+  var embeddedView, value;
+
+  view.lynxGetValue = function () {
+    return value;
+  };
+
+  view.lynxGetEmbeddedView = function () {
+    return embeddedView;
+  };
+
+  view.lynxSetEmbeddedView = function (newView, newBlob) {
+    var detached = [];
+
+    if (embeddedView) {
+      detached.push(view.removeChild(embeddedView));
+    }
+
+    embeddedView = newView;
+    value = newBlob;
+
+    if (!embeddedView) return detached;
+
+    view.appendChild(embeddedView);
+    embeddedView.setAttribute("data-lynx-embedded-view", true);
+
+    if (node.value.alt) {
+      embeddedView.setAttribute("alt", node.value.alt);
+    }
+
+    return detached;
+  };
+
+  var imageView = document.createElement("img");
+  imageView.src = url.resolve(node.base || "", node.value.src);
+  view.lynxSetEmbeddedView(imageView, getBlob(node));
+
+  return Promise.resolve(applyImageAttributes(node)(view));
+}
+
+export function imageViewBuilder(node) {
+  if (node.value.src) return buildAsEmbeddedImageTag(node);
+  else return contentViewBuilder(node).then(applyImageAttributes(node));
 }
