ca225e226ec85b627b6e0de4425c7970fd77277e
John Howes <john.howes@gmail.com>
2017-05-30 10:11:40 -0500

    Modified `textViewBuilder` to return the value of the `pre` Content Element

--- test/builders/text-view-builder.js
+++ test/builders/text-view-builder.js
@@ -9,10 +9,25 @@ describe("builders / textViewBuilder", function () {
     var node = {
       value: "Hello, World!"
     };
-    
+
     var view = builders.textViewBuilder(node);
-    
+
     expect(view).to.not.be.null;
     view.textContent.should.equal(node.value);
+  });
+
+  it("should maintain the text value as the layout container is modified.", function () {
+    var node = {
+      value: "Hello, World!"
+    };
+
+    var view = builders.textViewBuilder(node);
+
+    var presentationView = document.createElement("div");
+    presentationView.textContent = "Prefix";
+    view.appendChild(presentationView);
+
+    expect(view).to.not.be.null;
+    view.lynxGetValue().should.equal(node.value);
   });
 });
