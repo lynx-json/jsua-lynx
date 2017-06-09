215485b42d4249ac668617fd416dd4b2e154532d
Anson Goldade <anson.goldade@streamtext.net>
2017-06-09 16:18:53 -0500

    Revert text builder to 'pre'

--- src/builders/text-view-builder.js
+++ src/builders/text-view-builder.js
@@ -1,7 +1,6 @@
 export function textViewBuilder(node) {
   var view = document.createElement("div");
-  var textView = document.createElement("div");
-  textView.style.whiteSpace = "pre-wrap";
+  var textView = document.createElement("pre");
 
   view.appendChild(textView);
 
