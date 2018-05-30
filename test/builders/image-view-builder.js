0c984f1a97f2521c7e9a33966df111fc4f169bdf
Anson <anson.goldade@streamtext.net>
2018-05-30 10:49:57 -0500

    Set image dimensions at build time

--- test/builders/image-view-builder.js
+++ test/builders/image-view-builder.js
@@ -43,7 +43,9 @@ describe("builders / imageViewBuilder", function () {
       view.children.length.should.equal(1);
       view.children.item(0).getAttribute("alt").should.equal(node.value.alt);
       view.children.item(0).getAttribute("data-lynx-height").should.equal(node.value.height.toString());
+      view.children.item(0).height.should.equal(node.value.height);
       view.children.item(0).getAttribute("data-lynx-width").should.equal(node.value.width.toString());
+      view.children.item(0).width.should.equal(node.value.width);
       expect(buildStub.called).to.equal(true);
     });
   });
@@ -74,9 +76,40 @@ describe("builders / imageViewBuilder", function () {
       view.children.item(0).getAttribute("src").should.equal("http://example.com/foo");
       view.children.item(0).getAttribute("alt").should.equal(node.value.alt);
       view.children.item(0).getAttribute("data-lynx-height").should.equal(node.value.height.toString());
+      view.children.item(0).height.should.equal(node.value.height);
       view.children.item(0).getAttribute("data-lynx-width").should.equal(node.value.width.toString());
+      view.children.item(0).width.should.equal(node.value.width);
       transferStub.called.should.be.false;
       buildStub.called.should.be.false;
     });
   });
+
+  it("should return view for 'image' with 'data' and no dimensions set", function () {
+    var node = {
+      base: "http://example.com",
+      spec: {
+        hints: [{ name: "image" }, { name: "content" }]
+      },
+      value: {
+        data: "R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==",
+        encoding: "base64",
+        type: "image/jpeg",
+        alt: "A pastoral view"
+      }
+    };
+
+    buildStub.returns(Promise.resolve({ view: document.createElement("div"), content: { blob: {} } }));
+
+    return contents.imageViewBuilder(node).then(function (view) {
+      expect(view).to.not.be.null;
+      view.children.length.should.equal(1);
+      view.children.item(0).getAttribute("alt").should.equal(node.value.alt);
+      expect(view.children.item(0).getAttribute("data-lynx-height")).to.be.null;
+      expect(view.children.item(0).height).to.be.undefined;
+      expect(view.children.item(0).getAttribute("data-lynx-width")).to.be.null;
+      expect(view.children.item(0).width).to.be.undefined;
+      expect(buildStub.called).to.equal(true);
+    });
+  });
+
 });
